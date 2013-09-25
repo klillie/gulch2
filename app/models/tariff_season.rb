acff8051caf35557b3b572e83150371272a76943
J.S. Roy <jroy@chicagobooth.edu>
2013-09-25 12:02:49 -0500

    Add TOU request

--- app/models/tariff_season.rb
+++ app/models/tariff_season.rb
@@ -4,21 +4,12 @@ class TariffSeason < ActiveRecord::Base
 				foreign_key: "tariff_tou_id"
 	validates :tariff_territory_id, presence: true
 
+
+	# returns the type of season based on date and zip code
 	def self.season(date, zip)
 		@tariff_territory_id = TariffTerritory.territory_id(zip)
-		@season = TariffSeason.find_by(id: 4).season_type
-		@season = TariffSeason.find_by(tariff_territory_id: @tariff_territory_id).season_type
-		@season = TariffSeason.find_by(tariff_territory_id: @tariff_territory_id, start_date: date).season_type
-
-		#not working!
-		#@season = TariffSeason.where('id = ?', 1).season_type
-		#@season = TariffSeason.where(tariff_territory_id: 1).season_type
-		#@season = TariffSeason.where(id: 1).season_type
-		#@season = TariffSeason.where("tariff_territory_id = :territory_id AND id = season_id", territory_id: @tariff_territory_id, season_id: 1).season_type
-		#@season = TariffSeason.where(season_id: 1).season_type
-		
-		#@season = TariffSeason.("tariff_territory_id = ?", @tariff_territory_id).season_type
-
+		@season = TariffSeason.where('season_type != ? AND tariff_territory_id = ? AND start_date <= ? AND end_date >= ?', 
+			'All', @tariff_territory_id, date, date)
 
 	end
 
